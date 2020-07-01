193d9370e46490c804a90b40a07c39596aac4ce0
Renan Moraes <rmoraes@f9c.com.br>
2020-07-01 15:48:36 -0300

    Ajuste playbook install-helm.yml

--- playbooks/scripts/kubeadm-cp-join.sh
+++ playbooks/scripts/kubeadm-cp-join.sh
@@ -1,3 +1,3 @@
-  kubeadm join k8s.home.lab:6443 --token r97qn9.p21r722bgfnxbs4i \
-    --discovery-token-ca-cert-hash sha256:924d85c25db74d1ffa54ddc07e861a6f9335d3be4d4f44759429f4922d46fcf2 \
-    --control-plane --certificate-key 353f0004d7f2a142b628d7de26328404a130d5a54ca694c508743d1af864b420
+  kubeadm join k8s.home.lab:6443 --token x8miqe.ewcutmgcyye7uxkw \
+    --discovery-token-ca-cert-hash sha256:556f0f49d737461c1280ac5ad17031949f4fb3911241d3c2c3032f11f87653b8 \
+    --control-plane --certificate-key 41bba00a9e6345df42ec6afe97a6eed43fcfd35950f00fbf0aeba82fb475bf14
--- playbooks/scripts/kubeadm-worker-join.sh
+++ playbooks/scripts/kubeadm-worker-join.sh
@@ -1,2 +1,2 @@
-kubeadm join k8s.home.lab:6443 --token r97qn9.p21r722bgfnxbs4i \
-    --discovery-token-ca-cert-hash sha256:924d85c25db74d1ffa54ddc07e861a6f9335d3be4d4f44759429f4922d46fcf2 
+kubeadm join k8s.home.lab:6443 --token x8miqe.ewcutmgcyye7uxkw \
+    --discovery-token-ca-cert-hash sha256:556f0f49d737461c1280ac5ad17031949f4fb3911241d3c2c3032f11f87653b8 
